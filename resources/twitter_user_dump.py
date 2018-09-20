e220bd61755209573eee884c1ce5e2f3ddef3d52
Travis CI <garridolabs@gmail.com>
2018-09-20 11:02:45 +0000

    PEP8 Fix

--- resources/twitter_user_dump.py
+++ resources/twitter_user_dump.py
@@ -46,14 +46,15 @@ class TwitterUtils():
         try:
             # save the id of the oldest tweet less one
             oldest = alltweets[-1].id - 1
-        except:
+        except BaseException:
             return False
 
         # keep grabbing tweets until there are no tweets left to grab
         while len(new_tweets) > 0:
             print("getting tweets before %s" % (oldest))
 
-            # all subsiquent requests use the max_id param to prevent duplicates
+            # all subsiquent requests use the max_id param to prevent
+            # duplicates
             new_tweets = api.user_timeline(
                 screen_name=screen_name, count=200,
                 tweet_mode='extended', max_id=oldest)
