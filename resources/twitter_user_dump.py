306386d82ce64a7e5ebdef43bb15057e46e11e35
Juan-Cruz <juan.cruz@ibm.com>
2018-09-20 13:07:34 +0200

    Modifying copyright header

--- resources/twitter_user_dump.py
+++ resources/twitter_user_dump.py
@@ -1,6 +1,12 @@
-#!/usr/bin/env python
 # encoding: utf-8
+'''
+    @license
 
+    Copyright(c) 2018, GarridoLabs and the project's contributors.
+
+    This source code is licensed under the Apache License, Version 2.0 found in
+    the LICENSE.txt file in the root directory of this source tree.
+'''
 import tweepy  # https://github.com/tweepy/tweepy
 import csv
 import json
--- test/test_twitter_user_dump.py
+++ test/test_twitter_user_dump.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 '''
     @license
 
-    Copyright(c) 2018, IBM.
+    Copyright(c) 2018, GarridoLabs and the project's contributors.
 
     This source code is licensed under the Apache License, Version 2.0 found in
     the LICENSE.txt file in the root directory of this source tree.
