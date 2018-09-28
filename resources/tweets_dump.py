2eea11549e55b91028ca290fe3b10290f73fb4a3
Héctor Abraham Morillo Prieto <hectorabrahammp@gmail.com>
2018-09-28 19:41:17 +0200

    Refactored tweets dump from scrapping
    Added unit test and a small refactor of the code

--- resources/tweets_dump.py
+++ resources/tweets_dump.py
@@ -1,18 +1,24 @@
-import tweepy
+import csv
 import json
 import math
-import glob
-import csv
-import zipfile
-import zlib
 import sys
-from tweepy import TweepError
 from time import sleep
 
+import tweepy
 
-class TweetsDump():
-    def __init__(self, credentialsPath):
-        with open(credentialsPath, "r") as data_file:
+'''
+    @license
+
+    Copyright(c) 2018, GarridoLabs and the project's contributors.
+
+    This source code is licensed under the Apache License, Version 2.0 found in
+    the LICENSE.txt file in the root directory of this source tree.
+'''
+
+
+class TweetsDump:
+    def __init__(self, credentials_path):
+        with open(credentials_path, "r") as data_file:
             data_loaded = json.load(data_file)
 
         # Twitter API credentials
@@ -21,16 +27,16 @@ class TweetsDump():
         self.access_key = data_loaded['accessToken']
         self.access_secret = data_loaded['accessTokenSecret']
 
-    def is_retweet(self, entry):
-        return 'retweeted_status' in entry.keys()
+        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        auth.set_access_token(self.access_key, self.access_secret)
+        self.api = tweepy.API(auth)
 
-    def get_source(self, entry):
-        if '<' in entry["source"]:
-            return entry["source"].split('>')[1].split('<')[0]
-        else:
-            return entry["source"]
+    @staticmethod
+    def is_retweet(tweet):
+        return tweet["retweeted"]
 
-    def get_ids(self, ids_input_file):
+    @staticmethod
+    def get_ids(ids_input_file):
         with open(ids_input_file) as f:
             ids = json.load(f)
 
@@ -38,7 +44,20 @@ class TweetsDump():
 
         return ids
 
-    def retrieve_tweets(self, api, ids):
+    @staticmethod
+    def write_non_retweeted_tweets_plain_text(output_file, tweets):
+        f = csv.writer(open(output_file, 'w'), delimiter='\n', quoting=csv.QUOTE_NONE)
+        for tweet in tweets:
+            if not TweetsDump.is_retweet(tweet):
+                f.writerow([tweet["text"]])
+
+    @staticmethod
+    def write_tweets_json(output_file, tweets):
+        print('creating master json file')
+        with open(output_file, 'w') as outfile:
+            json.dump(tweets, outfile)
+
+    def retrieve_tweets(self, ids):
         all_tweets = []
         start = 0
         end = 100
@@ -51,39 +70,25 @@ class TweetsDump():
             id_batch = ids[start:end]
             start += 100
             end += 100
-            tweets = api.statuses_lookup(id_batch)
+            tweets = self.api.statuses_lookup(id_batch)
             for tweet in tweets:
                 all_tweets.append(dict(tweet._json))
 
         print('metadata collection complete')
         return all_tweets
 
-    def write_tweets_json(self, output_file, tweets):
-        print('creating master json file')
-        with open(output_file, 'w') as outfile:
-            json.dump(tweets, outfile)
-
-    def write_tweets_plain_text(self, output_file, tweets):
-        f = csv.writer(open(output_file, 'w'))
-        outtweets = []
-        for tweet in tweets:
-            if not tweet["retweeted"]:
-                f.writerow([tweet["text"]])
-
     def dump_all_tweets(self, ids_input_file, output_base_name):
-        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
-        auth.set_access_token(self.access_key, self.access_secret)
-        api = tweepy.API(auth)
+
 
         output_base_name = output_base_name.lower()
         output_file = '{}.json'.format(output_base_name)
         output_file_txt = '{}.txt'.format(output_base_name)
 
         ids = self.get_ids(ids_input_file)
-        all_data = self.retrieve_tweets(api, ids)
+        all_data = self.retrieve_tweets(ids)
 
         self.write_tweets_json(output_file, all_data)
-        self.write_tweets_plain_text(output_file_txt, all_data)
+        self.write_non_retweeted_tweets_plain_text(output_file_txt, all_data)
 
 
 if __name__ == '__main__':
